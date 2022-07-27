8a14a3faf73281a1d1ff3fc0ca117f440b4f2fd2
indraawagin <indraawagin@gmail.com>
2022-07-27 13:40:59 +0800

    fix verify email

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')
     ->name('home');
-    
+
 Route::get('/detail/{slug}', 'DetailController@index')
     ->name('detail');
 
@@ -47,10 +47,10 @@ Route::prefix('admin')
     {
         Route::get('/','DashboardController@index')
             ->name('dashboard');
-        
+
         Route::resource('travel-package', 'TravelPackageController');
         Route::resource('gallery', 'GalleryController');
         Route::resource('transaction', 'TransactionController');
     });
 
-Auth::routes(['verify' => true]);
+Auth::routes();
